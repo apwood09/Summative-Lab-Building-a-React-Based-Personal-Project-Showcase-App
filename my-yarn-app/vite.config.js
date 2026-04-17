9540ea91f952c10a7274b4ddee3835c231b5f379
abbi portwood <apwood09@gmail.com>
2026-04-17 09:16:19 -0400

    added to test with vite

--- my-yarn-app/vite.config.js
+++ my-yarn-app/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+  globals: true,
+  environment: 'jsdom',
+  setupFiles: './src/setupTests.js',
+},
+
   plugins: [react()],
   server: {
     port: 5173,
